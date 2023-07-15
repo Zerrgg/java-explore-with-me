ab2cf821f5dcf05c576a95365f3a654cf52df002
Zerrgg <GIZMO2684@yandex.ru>
2023-07-16 00:07:25 +0300

    add stats service V.1.3

--- stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -1,6 +1,5 @@
 package ru.practicum.client;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
